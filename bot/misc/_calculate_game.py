e97dcfe5aca33bccd42461fcd8c518cfd4937bdf
Th3Kanashii <Th3Kanashii@gmail.com>
2024-04-03 22:01:17 +0300

    Add annotation

--- bot/misc/_calculate_game.py
+++ bot/misc/_calculate_game.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Union
+from typing import Any, Dict, Union
 
 
 @lru_cache(maxsize=64)
@@ -10,7 +10,7 @@ def calculate_slot(bet_amount: int, dice_value: int) -> int:
     :param dice_value: dice value (1-64)
     :return: user score change (integer)
     """
-    win_multipliers = {1: 4, 22: 4, 43: 4, 16: 3, 32: 3, 48: 3, 64: 5}
+    win_multipliers: Dict[int, int] = {1: 4, 22: 4, 43: 4, 16: 3, 32: 3, 48: 3, 64: 5}
     return bet_amount * win_multipliers.get(dice_value, -1)
 
 
@@ -23,7 +23,7 @@ def calculate_basketball_football(bet_amount: int, dice_value: int) -> Union[int
     :param dice_value: The value rolled on the dice (1-5).
     :return: The amount won by the user.
     """
-    win_multipliers = {4: 1.5, 5: 2.5}
+    win_multipliers: Dict[int, float] = {4: 1.5, 5: 2.5}
     return bet_amount * win_multipliers.get(dice_value, -1)
 
 
@@ -36,5 +36,5 @@ def calculate_dice_dart_bowling(bet_amount: int, dice_value: int) -> Union[int, 
     :param dice_value: The value rolled on the dice (1-6).
     :return: The amount won by the user.
     """
-    win_multipliers = {4: 1.5, 5: 2, 6: 3}
+    win_multipliers: Dict[int, Any] = {4: 1.5, 5: 2, 6: 3}
     return bet_amount * win_multipliers.get(dice_value, -1)
